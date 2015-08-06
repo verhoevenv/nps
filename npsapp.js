3c482a92a500eee557645728b3230db46664dd07
Vincent Verhoeven <verhoevenv@gmail.com>
2015-08-06 20:45:10 +0200

    Made input fields numeric

--- npsapp.js
+++ npsapp.js
@@ -2,6 +2,10 @@ angular.module('NPSApp', [])
 .controller('NPSController', function() {
 	var model = this;
 
+	model.critics = 0;
+	model.neutrals = 0;
+	model.promotors = 0;
+
 	model.nps = function() {
 		var total = parseInt(model.critics,10)+parseInt(model.neutrals,10)+parseInt(model.promotors,10);
 		return Math.round(model.promotors/total*100) - Math.round(model.critics/total*100);
